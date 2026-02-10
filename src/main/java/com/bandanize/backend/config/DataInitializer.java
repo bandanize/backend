a4cc93d3cb53f1396aca911772881e406e5ffbe6
r-dvl <rauldel.valle.lima@gmail.com>
2026-02-10 16:32:04 +0100

    Remove admin user autogeneration

--- src/main/java/com/bandanize/backend/config/DataInitializer.java
+++ src/main/java/com/bandanize/backend/config/DataInitializer.java
@@ -1,6 +1,5 @@
 package com.bandanize.backend.config;
 
-import com.bandanize.backend.models.UserModel;
 import com.bandanize.backend.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -13,18 +12,7 @@ public class DataInitializer {
     @Bean
     public CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            // Create admin user
-            if (userRepository.count() == 0) {
-                UserModel user = new UserModel();
-                user.setName("Raul Del Valle");
-                user.setUsername("admin");
-                user.setEmail("admin@bandanize.com");
-                user.setCity("Tarifa");
-                user.setHashedPassword(passwordEncoder.encode("admin"));
-                user.setDisabled(false);
-                userRepository.save(user);
-                System.out.println("Usuario inicial creado: " + user.getUsername());
-            }
+            // No initial data required
         };
     }
 }
